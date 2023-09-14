6a6c838d83cd4c849b359467f940ebcbb6c2257b
joseMeneghetti <jrmp.dev@gmail.com>
2023-09-13 22:39:03 -0300

    fix env public

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["oaidalleapiprodscus.blob.core.windows.net"],
   },
+  env: {
+    BACK_END_URL: process.env.BACK_END_URL,
+  },
 };
 
 module.exports = nextConfig;
